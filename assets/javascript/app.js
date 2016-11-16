f123876e00986c21eaac32511c3ece6d3fc7c6eb
drichard1989 <drichard1989@icloud.com>
2016-11-16 15:18:54 -0600

    Added the functions that will kick it to the next page if the time runs out or if the button at the end of the quiz is clicked.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,17 +10,22 @@ var incorrectAnswer = 0;
 var timeAllowed = 95;
 var counter ;
 
+
+// This function sets the formula to reduce at a rate of 1 second per reduction
 function run() {
 	counter = setInterval(decrement, 1000);
 };
 
+
+// This function reduces the time, and at 0, runs the funcion that will kick it to the next page. 
 function decrement () {
 	timeAllowed--;
 	$("#timeAllowed").html("<h2>" + timeAllowed + "</h2>");
-};
 
-	// Me playing with the fadeIn method. Not working...
-	// $("#headerRow").fadeIn(1000);
+	if (timeAllowed == 0) {
+		startTheKick();
+	}
+};
 
 	// Here, on the click of the go under button, this will start the quiz,
 	// music will play, it will hide the intro section and show the quiz section.
@@ -30,14 +35,21 @@ function decrement () {
 		console.log("click");
 		$("#headerRow").fadeOut(1000);
 		$("#quizSection").delay(1000).fadeIn(7000);
-		
-
-
-
-		 // Note -- When I have this running, the quiz dissapears after 1 second. 
 		run();
 
 
+	});
+
+	// This function will send me to the next page. This funciton I think should calculate the results.
+	function startTheKick(){
+		$("#quizSection").fadeOut(1000);
+		$("#resultsSection").delay(1000).fadeIn(5000);
+	}
+
+	// This will move the game to the results page by running the function startTheKick if the button is clicked.
+	$(document).on("click", "#startTheKick", function(){
+		startTheKick();
+		
 	});
 
 
