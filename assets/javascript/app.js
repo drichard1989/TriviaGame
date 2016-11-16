29be973fdffd1aadb8f5f5e003b704cc258440c2
drichard1989 <drichard1989@icloud.com>
2016-11-16 08:43:51 -0600

    I have finished the quiz html, and I have attempted to write the funtions for the timer, and it will not work.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,42 @@
-var dreamMusic = new Audio(src = "assets/music/dreamMusic.mp3")
+
 
 
 $(document).ready(function(){
 
+// All the variables I will need
+var dreamMusic = new Audio(src = "assets/music/dreamMusic.mp3")
+var correctAnswer = 0;
+var incorrectAnswer = 0;
+var timeAllowed = 90;
+var counter ;
+
+function run() {
+	counter = setInterval(decrement, 1000);
+};
+
+function decrement () {
+	timeAllowed--;
+	$("#quizSection").html("<h2>" + timeAllowed + "</h2>");
+};
+
+	// Me playing with the fadeIn method. Not working...
+	// $("#headerRow").fadeIn(1000);
+
+	// Here, on the click of the go under button, this will start the quiz,
+	// music will play, it will hide the intro section and show the quiz section.
 	$(document).on("click", "#goUnderButton", function(){
 
 		dreamMusic.play();
 		console.log("click");
-		$("#quizSection").removeClass("hidden");
+		$("#quizSection").removeClass("hidden").fadeIn(1000);
 		$("#headerRow").addClass("hidden");
+
+
+
+		 // Note -- When I have this running, the quiz dissapears after 1 second. 
+		run();
+
+
 	});
 
 
